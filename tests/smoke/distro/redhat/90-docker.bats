3b67b15b6b9ee0abf321336c3483dfe4da4d8221
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2025-12-17 18:15:50 +0100

    Task 03: Fix test setup (test all builds)

--- tests/smoke/distro/redhat/90-docker.bats
+++ tests/smoke/distro/redhat/90-docker.bats
@@ -1,11 +1,5 @@
 #!/usr/bin/env bats
 
-load "${BATS_TEST_DIRNAME}/../../helpers/os.bash"
-
-setup_file() {
-  require_fedora
-}
-
 @test "docker cli present" {
   run docker run --rm \
     "${AICAGE_IMAGE_BASE_IMAGE}" \
--- tests/smoke/helpers/os.bash
+++
@@ -1,9 +0,0 @@
-require_fedora() {
-  run docker run --rm \
-    "${AICAGE_IMAGE_BASE_IMAGE}" \
-    /bin/bash -lc "source /etc/os-release && echo ${ID}"
-  [ "$status" -eq 0 ]
-  if [[ "${output}" != "fedora" ]]; then
-    skip "Fedora-only test"
-  fi
-}
